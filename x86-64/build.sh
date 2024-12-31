c91f81d082bad4184d72e3ba560a5a199dac7ab2
sinoserendipity <sinoserendipity@gmail.com>
2024-12-30 19:03:00 -0500

    build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -17,12 +17,9 @@ PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-app-vnstat2"
-PACKAGES="$PACKAGES luci-app-filebrowser-go"
 
 
 # 构建镜像
