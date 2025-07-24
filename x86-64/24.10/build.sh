44d351f3f539d2569975eedc7c9cfc2a77ba51ea
sinoserendipity <sinoserendipity@gmail.com>
2025-07-24 14:24:35 -0400

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -69,6 +69,9 @@ PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 # 流量监控器
 PACKAGES="$PACKAGES luci-app-vnstat2"
 PACKAGES="$PACKAGES luci-i18n-vnstat2-zh-cn"
+# cloudflared
+PACKAGES="$PACKAGES luci-app-cloudflared"
+PACKAGES="$PACKAGES luci-i18n-cloudflared-zh-cn"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
